90f8dad927104d6217a0a66ce41bb77c39bb9010
Nariman <mirmanov1@mail.ru>
2024-02-01 10:13:14 -0800

    add feedback form

--- src/components/FeedSection/FeedSection.jsx
+++ src/components/FeedSection/FeedSection.jsx
@@ -1,9 +1,28 @@
+import Button from '../Button/Button';
 import style from './FeedSection.module.css';
 
 const FeedSection = () => {
   return (
-    <section className={style['feedback']}>
-      <h3>Обратная связь</h3>
+    <section>
+      <h3 className={style['feedback']}>Обратная связь</h3>
+      <form>
+        <label htmlFor='name'>Ваше имя</label>
+        <input
+          type='text'
+          id='name'
+          className={style['control']}
+        />
+      </form>
+      <label htmlFor='reason'>Причина обращения</label>
+      <select
+        id='reason'
+        className={style['control']}
+      >
+        <option value='error'>Ошибка</option>
+        <option value='help'>Помощь</option>
+        <option value='suggest'>Предложение</option>
+      </select>
+      <Button>Отправить</Button>
     </section>
   );
 };
