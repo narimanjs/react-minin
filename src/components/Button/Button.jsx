73c684d9c485f6f73120dd81745d8583dd5510da
Nariman <mirmanov1@mail.ru>
2024-02-04 22:30:32 -0800

    add Intro

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,12 @@
 import styles from './Button.module.css';
 import cn from 'classnames';
-function Button({ children, onClikkk, isActive }) {
+function Button({ children, onClikkk, isActive, disabled, ...props }) {
   return (
     <button
+      {...props}
       className={cn(styles.button, { [styles.active]: isActive })}
       onClick={onClikkk}
+      disabled={disabled}
     >
       {children}
     </button>
--- src/components/IntroSection/IntroSection.jsx
+++ src/components/IntroSection/IntroSection.jsx
@@ -1,5 +1,28 @@
 import styles from './IntroSection.module.css';
 function IntroSection() {
+  // Promise.reject('a')
+  //   .catch(p => p + 'b')
+  //   .catch(p => p + 'c')
+  //   .then(p => p + 'd')
+  //   .finally(p => p + 'e')
+  //   .then(p => console.log(p));
+
+  const obj = {
+    x: 10,
+    y: 20,
+  };
+  const prop = Object.keys(obj).reverse().join(' ');
+  obj.xy = 30;
+  obj.yx = 40;
+  obj.x10y20 = 50;
+  obj.y20x10 = 60;
+  const result = obj[prop];
+  console.log(result);
+  console.log(0 || (1 && 2) || 3);
+
+  function foo() {}
+  foo.x = 10;
+  console.log('foo', foo.x);
   return (
     <section>
       <h1 className={styles['centered']}>Nariman University</h1>
@@ -7,6 +30,14 @@ function IntroSection() {
         Университет frontend-разработки, который насыщает IT-сферу
         квалифицированными программистами
       </h3>
+      <div className={styles['parent']}>
+        block 1<div className={styles['block2']}>block 2</div>
+      </div>
+      <div className={styles['flex']}>
+        <div>elem 1</div>
+        <div>elem 2</div>
+        <div>elem 3</div>
+      </div>
     </section>
   );
 }
